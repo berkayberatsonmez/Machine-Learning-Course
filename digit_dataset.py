a55932bd65e787e01644b71a232969e38492dd8a
BBS <60455005+berkayberatsonmez@users.noreply.github.com>
2022-03-01 03:28:01 +0300

    Update digit_dataset.py

--- digit_dataset.py
+++ digit_dataset.py
@@ -6,26 +6,26 @@ from sklearn.datasets import load_digits
 import matplotlib.pyplot as plt
 import pandas as pd
 
-digits = datasets.load_digits(n_class=8,as_frame=True)
-print(digits.data.shape)
+digits1 = datasets.load_digits(n_class=8,as_frame=True)
+print(digits1.data.shape)
 
 plt.figure(1, figsize=(5,5), facecolor=('red'), edgecolor=('blue'))
-plt.imshow(digits.images[5], cmap=plt.cm.gray_r, interpolation="bicubic")
+plt.imshow(digits1.images[5], cmap=plt.cm.gray_r, interpolation="bicubic")
 plt.show()
-***********************************************************************************************************************************************************************************
+
 digits2 = load_digits()
 print(digits2.data.shape)
 
 plt.gray()
 plt.matshow(digits2.images[0])
 plt.show()
-***********************************************************************************************************************************************************************************
-digits = pd.DataFrame(datasets.load_digits().data)
-digits['target'] = datasets.load_digits().target
-digits.tail()
 
-digits_matrix = datasets.load_digits().data
+digits3 = pd.DataFrame(datasets.load_digits().data)
+digits3['target'] = datasets.load_digits().target
+digits3.tail()
+
+digits3_matrix = datasets.load_digits().data
 
 plt.figure(figsize=(10,10))
-plt.imshow(digits_matrix[0].reshape(8,8))
+plt.imshow(digits3_matrix[0].reshape(8,8))
 plt.show()
