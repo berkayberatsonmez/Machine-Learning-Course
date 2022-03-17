80fa286877deb32d308beb5e5747d1864b060908
BBS <60455005+berkayberatsonmez@users.noreply.github.com>
2022-03-17 08:37:57 +0300

    Update confusion_matrix.py

--- confusion_matrix.py
+++ confusion_matrix.py
@@ -56,7 +56,7 @@ x = (x_data - np.min(x_data))/(np.max(x_data)-np.min(x_data))
 #%%
 # train test split
 from sklearn.model_selection import train_test_split
-x_train, x_test, y_train, y_test = train_test_split(x,y,test_size = 0.3,random_state=8)
+x_train, x_test, y_train, y_test = train_test_split(x,y,test_size = 0.4,random_state=8)
 
 
 
